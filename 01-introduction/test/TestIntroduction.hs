02e3d893b6d83b3e48127d3f048e1bae106a74a6
David Pritchard <david.al.pritchard@gmail.com>
2022-10-25 08:59:44 -0400

    set up chapter 1 testing

---
+++ 01-introduction/test/TestIntroduction.hs
@@ -0,0 +1,13 @@
+module Main (main) where
+
+import           Test.Tasty                     ( TestTree
+                                                , defaultMain
+                                                , testGroup
+                                                )
+import           Test.Tasty.HUnit               ( (@=?)
+                                                , (@?)
+                                                , testCase
+                                                )
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented."
