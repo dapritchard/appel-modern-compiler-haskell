dae80de48a580455c5526aa0399a829edae6b584
brendanrbrown <brendanrbrown@runbox.com>
2024-04-07 16:27:31 -0400

    fresh start for the exercise, using existing organization

--- 01-introduction/src/StraightLineProgramInterpreter.hs
+++ 01-introduction/src/StraightLineProgramInterpreter.hs
@@ -3,19 +3,21 @@ pages 10-12.
 
 Run `interp prog` in GHCi to try it out.
 -}
+{-# LANGUAGE StrictData #-}
+
 module StraightLineProgramInterpreter
-  ( Binop (..)
-  , Exp (..)
-  , Id (..)
-  , Stm (..)
-  , Table
-  , interp
-  , prog
-  ) where
+  ( Binop (..),
+    Exp (..),
+    Id (..),
+    Stm (..),
+    Table,
+    interp,
+    prog,
+  )
+where
 
-import Data.Map.Strict ( Map, empty, findWithDefault, insert )
-import Data.Text ( Text )
-
+import Data.Map.Strict (Map, empty, findWithDefault, insert)
+import Data.Text (Text)
 
 -- Types -----------------------------------------------------------------------
 
@@ -28,27 +30,24 @@ type Id = Text
 
 data Binop = Plus | Minus | Times | Div
 
-data Stm =
-    CompoundStm !Stm !Stm
-  | AssignStm !Id !Exp
-  | PrintStm ![Exp]
+data Stm
+  = CompoundStm Stm Stm
+  | AssignStm Id Exp
+  | PrintStm [Exp]
 
-data Exp =
-    IdExp !Text
-  | NumExp !Integer
-  | OpExp !Exp !Binop !Exp
-  | EseqExp !Stm !Exp
+data Exp
+  = IdExp Text
+  | NumExp Integer
+  | OpExp Exp Binop Exp
+  | EseqExp Stm Exp
 
 type Table = Map Text Integer
-
 
 -- Interpreter -----------------------------------------------------------------
 
 -- Interpret a new program starting with an empty environment
 interp :: Stm -> IO ()
-interp s = do
-  interpStm s empty
-  return ()
+interp s = undefined
 
 {- Interpret an expression with regards to a given environment using the types
 suggested on page 11. Two defects of this interpreter component are that:
@@ -57,39 +56,14 @@ is used
 2. If the program divides by 0 then the interpreter will throw an exception
 -}
 interpExp :: Exp -> Table -> IO (Integer, Table)
-interpExp (IdExp v) t = return (findWithDefault 0 v t, t)
-interpExp (NumExp x) t = return (x, t)
-interpExp (OpExp e1 binop e2) t = do
-  (x1, newT1) <- interpExp e1 t
-  (x2, newT2) <- interpExp e2 newT1
-  case binop of
-    Plus -> return (x1 + x2, newT2)
-    Minus -> return (x1 - x2, newT2)
-    Times -> return (x1 * x2, newT2)
-    Div -> return (x1 `quot` x2, newT2)
-interpExp (EseqExp s e) t = do
-  newT <- interpStm s t
-  interpExp e newT
+interpExp = undefined
 
 {- Interpret a statment with regards to a given environment using the types
 suggested on page 11. The `PrintStm` case cheats a little bit by printing a
 trailing space at the end of the line.
 -}
 interpStm :: Stm -> Table -> IO Table
-interpStm (CompoundStm s1 s2) t = do
-  newT <- interpStm s1 t
-  interpStm s2 newT
-interpStm (AssignStm id e) t = do
-  (v, newT) <- interpExp e t
-  return (insert id v newT)
-interpStm (PrintStm []) t = do
-  putStrLn ""
-  return t
-interpStm (PrintStm (e:es)) t = do
-  (v, newT) <- interpExp e t
-  putStr $ show v ++ " "
-  interpStm (PrintStm es) newT
-
+interpStm = undefined
 
 -- Data ------------------------------------------------------------------------
 
@@ -105,15 +79,15 @@ topLevelLHS = AssignStm "a" (OpExp (NumExp 5) Plus (NumExp 3))
 topLevelRHS :: Stm
 topLevelRHS =
   CompoundStm
-    (AssignStm
-      "b"
-      (EseqExp
-        (PrintStm [
-            IdExp "a"
-          , OpExp (IdExp "a") Minus (NumExp 1)
-          ]
+    ( AssignStm
+        "b"
+        ( EseqExp
+            ( PrintStm
+                [ IdExp "a",
+                  OpExp (IdExp "a") Minus (NumExp 1)
+                ]
+            )
+            (OpExp (NumExp 10) Times (IdExp "a"))
         )
-        (OpExp (NumExp 10) Times (IdExp "a"))
-      )
     )
     (PrintStm [IdExp "b"])
