a86443932d527894cf102bf4fd07f891b522d25f
David Pritchard <david.al.pritchard@gmail.com>
2024-05-27 08:18:42 -0400

    update parser tests

--- 02-lexical-analysis/test/Main.hs
+++ 02-lexical-analysis/test/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Test.Tasty (defaultMain, testGroup)
 import TestLexicalAnalysis (lexerAlexTests)
+import TestTigerParserHappy (parserHappyTests)
 
 main :: IO ()
-main = defaultMain $ testGroup "all tests" [lexerAlexTests]
+main = defaultMain $ testGroup "all tests" [lexerAlexTests, parserHappyTests]
